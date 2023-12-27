e5367f55f76ed748f925908145ccea89b0560ebb
Jake <11488599+vingard@users.noreply.github.com>
2023-12-27 12:44:14 +0000

    final redis commit (going to libsql turso!)

--- src/middleware/redis.ts
+++ src/middleware/redis.ts
@@ -2,6 +2,6 @@ import { Context, Next } from "hono"
 import { getRedis } from "../util/util"
 
 export async function redis(ctx: Context, next: Next) {
-    (<any>ctx).req.redis = getRedis(ctx)
+    ctx.req.redis = getRedis(ctx)
     await next()
 }
